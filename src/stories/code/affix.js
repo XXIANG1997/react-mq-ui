53d5d570c4b52da4ef7dbbd9561ebc2d0314dd00
xxiang1997 <moldysaltedfish@gmail.com>
2020-03-05 09:23:16 +0800

    storybook: add affix

---
+++ src/stories/code/affix.js
@@ -0,0 +1,123 @@
+import React from "react";
+import MqAffix from "../../components/affix";
+import { MqButton } from "../../components/button";
+
+const Affix = () => (
+	<div>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<MqAffix topOffset={0}>
+			<MqButton>affix 固钉组件</MqButton>
+		</MqAffix>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<MqAffix topOffset={100}>
+			<MqButton>affix 固钉组件(topOffset = 100)</MqButton>
+		</MqAffix>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<MqAffix bottomOffset={100}>
+			<MqButton>affix 固钉组件 (bottomOffset = 100)</MqButton>
+		</MqAffix>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+	</div>
+);
+
+export default Affix;
--- src/stories/navigation.stories.js
+++ src/stories/navigation.stories.js
@@ -2,8 +2,10 @@ import { storiesOf } from "@storybook/react";
 import breadcrumb from "./code/breadcrumb";
 import pagination from "./code/pagination";
 import dropdown from "./code/dropdown";
+import Affix from "./code/affix";
 
 storiesOf("导航", module)
 	.add("breadcrumb 面包屑", breadcrumb)
 	.add("pagination 分页器", pagination)
-	.add("dropdown 下拉菜单", dropdown);
+	.add("dropdown 下拉菜单", dropdown)
+	.add("affix 固钉", Affix);
