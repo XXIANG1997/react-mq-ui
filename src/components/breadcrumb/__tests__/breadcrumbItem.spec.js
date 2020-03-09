17e76ae6300ef3ca414476739ecec9184b4bb309
xxiang1997 <moldysaltedfish@gmail.com>
2020-03-09 14:12:13 +0800

    breadcrumbItem: optimize test code

--- src/components/breadcrumb/__tests__/breadcrumbItem.spec.js
+++ src/components/breadcrumb/__tests__/breadcrumbItem.spec.js
@@ -6,11 +6,21 @@ describe("<BreadCrumbItem />", () => {
 
 	test("allow us set props", () => {
 		const wrapper = mount(
-			<BreadcrumbItem separator={">"} icon={"home"} active={true}>test</BreadcrumbItem>
+			<BreadcrumbItem separator={"/"} icon={"home"} active={true}>one</BreadcrumbItem>
 		);
-		expect(wrapper.props().children).toBe("test");
-		expect(wrapper.props().separator).toBe(">");
+		expect(wrapper.props().children).toBe("one");
+		expect(wrapper.props().separator).toBe("/");
 		expect(wrapper.props().active).toBe(true);
 		expect(wrapper.props().icon).toBe("home");
+		wrapper.setProps({
+			separator: ">",
+			icon: "tag",
+			active: false,
+			children: "two"
+		});
+		expect(wrapper.props().children).toBe("two");
+		expect(wrapper.props().separator).toBe(">");
+		expect(wrapper.props().active).toBe(false);
+		expect(wrapper.props().icon).toBe("tag");
 	});
 });
