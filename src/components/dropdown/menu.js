6840f93c581f38c36c7aebe691fe37a9eb5b306d
xxiang1997 <moldysaltedfish@gmail.com>
2020-03-11 09:34:25 +0800

    dropdown: support callback

--- src/components/dropdown/menu.js
+++ src/components/dropdown/menu.js
@@ -1,24 +1,40 @@
 import React from "react";
+import propTypes from "prop-types";
 
-class Menu extends React.Component {
-
-	constructor(props) {
-		super(props);
-	}
-
-	static Item(props) {
-		const prefixItemCls = "mq-menu-item";
-		return <li className={prefixItemCls}>
-			{props.children}
-		</li>;
-	}
-
-	render() {
-		const prefixCls = "mq-menu";
-		return <ul className={prefixCls}>
-			{this.props.children}
-		</ul>;
-	}
+function Menu(props) {
+	const prefixCls = "mq-menu";
+	return <ul className={prefixCls}>
+		{props.children.map((child, index) => {
+			return React.cloneElement(child, {_callback: props.callback, key: index});
+		})}
+	</ul>;
 }
+
+Menu.propTypes = {
+	/** callback 函数 */
+	callback: propTypes.func
+};
+
+Menu.defaultProps = {
+	callback: () => {
+	}
+};
+
+Menu.Item = (props) => {
+	const prefixItemCls = "mq-menu-item";
+	return <li onClick={() => props._callback(props.name)} className={prefixItemCls}>
+		{props.children}
+	</li>;
+};
+
+Menu.Item.propTypes = {
+	/** callback 函数（父组件传过来） */
+	_callback: propTypes.func,
+	/** 触发回调时的 id */
+	name: propTypes.oneOfType([
+		propTypes.string,
+		propTypes.number
+	])
+};
 
 export default Menu;
