baa30bfbb7dc86162c5d0825ed83e7ba13f979ca
xxiang1997 <moldysaltedfish@gmail.com>
2020-03-19 09:05:45 +0800

    input: optimize code

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -48,6 +48,53 @@ class Input extends React.Component {
 		});
 	}
 
+	renderAddon = (addon, location, inputPrefix) => {
+		const hash = {
+			before: "addonBefore",
+			after: "addonAfter"
+		};
+		return addon && <div className={`${inputPrefix}-${hash[location]}`}>
+			{addon}
+		</div>;
+	};
+
+	renderIcon = (inputPrefix, prefix, suffix, showPassword, type, showCloseEye, allowClear, value) => {
+		if (prefix) {
+			return <span className={`${inputPrefix}-prefix`}>
+					<svg className={"mq-icon"} aria-hidden="true">
+						<use xlinkHref={`#icon-${prefix}`}/>
+					</svg>
+			</span>;
+		}
+
+		if (showPassword && type === "password") {
+			return <span className={`${inputPrefix}-suffix`}>
+					<svg onClick={this.triggerPasswordIcon} className={"mq-icon is-showPassword-icon"}
+					     aria-hidden="true">
+						<use xlinkHref={`#icon-${showCloseEye ? "close-eye" : "eye"}`}/>
+					</svg>
+			</span>;
+		}
+
+		if (allowClear) {
+			return (typeof value === "number" ? true : value) && allowClear &&
+				<span className={`${inputPrefix}-suffix`}>
+					<svg onClick={this.clearValue} className={"mq-icon is-clear-icon"}
+					     aria-hidden="true">
+						<use xlinkHref={"#icon-clear"}/>
+					</svg>
+				</span>;
+		}
+
+		if (suffix) {
+			return <span className={`${inputPrefix}-suffix`}>
+					<svg className={"mq-icon"} aria-hidden="true">
+						<use xlinkHref={`#icon-${suffix}`}/>
+					</svg>
+			</span>;
+		}
+	};
+
 	render() {
 		const inputPrefix = "mq-input";
 		const {type, placeholder, disabled, readOnly, addonBefore, addonAfter, size, allowClear, prefix, suffix, showPassword, width} = this.props;
@@ -57,76 +104,25 @@ class Input extends React.Component {
 			className: cls(inputPrefix, {
 				"have-prev-brother": !!addonBefore,
 				"have-next-brother": !!addonAfter,
-				"have-after-icon": allowClear || !!suffix,
-				"have-before-icon": !!prefix
+				"have-before-icon": !!prefix,
+				"have-after-icon": allowClear || !!suffix || !!(showPassword && type === "password")
 			}),
 			disabled,
 			readOnly,
-			value: value
+			value
 		};
-
 		// eslint-disable-next-line no-useless-escape
 		type === "email" && (attrs["pattern"] = "[a-z0-9._%+-]`+@[a-z0-9.-]+\.[a-z]{2,4}$");
 		placeholder && (attrs["placeholder"] = placeholder);
 
 		return <div className={`${inputPrefix}-wrapper ${inputPrefix}-${size}`}>
-			{
-				addonBefore && <div className={`${inputPrefix}-addonBefore`}>
-					{addonBefore}
-				</div>
-			}
+			{this.renderAddon(addonBefore, "before", inputPrefix)}
 			<div className={`${inputPrefix}-inner-wrapper`}>
-				{
-					prefix && <span className={`${inputPrefix}-prefix`}>
-						<svg className={"mq-icon"} aria-hidden="true">
-							<use xlinkHref={`#icon-${prefix}`}/>
-						</svg>
-					</span>
-				}
+				{this.renderIcon(inputPrefix, prefix)}
 				<input onChange={this._onChange} style={{"width": width + "px"}} {...attrs} />
-				{
-					showPassword && type === "password"
-						?
-						(
-							<span className={`${inputPrefix}-suffix`}>
-								<svg onClick={this.triggerPasswordIcon} className={"mq-icon is-showPassword-icon"}
-								     aria-hidden="true">
-									<use xlinkHref={`#icon-${showCloseEye ? "close-eye" : "eye"}`}/>
-								</svg>
-							</span>
-						)
-						:
-						(
-							allowClear
-								?
-								(
-									/**
-									 * 如果 value 是 number 直接返回 true 否则直接返回 value（string）
-									 */
-									(typeof value === "number" ? true : value) && allowClear &&
-									<span className={`${inputPrefix}-suffix`}>
-										<svg onClick={this.clearValue} className={"mq-icon is-clear-icon"}
-										     aria-hidden="true">
-											<use xlinkHref={"#icon-clear"}/>
-										</svg>
-									</span>
-								)
-								:
-								(
-									suffix && <span className={`${inputPrefix}-suffix`}>
-										<svg className={"mq-icon"} aria-hidden="true">
-											<use xlinkHref={`#icon-${suffix}`}/>
-										</svg>
-									</span>
-								)
-						)
-				}
+				{this.renderIcon(inputPrefix, prefix, suffix, showPassword, type, showCloseEye, allowClear, value)}
 			</div>
-			{
-				addonAfter && <div className={`${inputPrefix}-addonAfter`}>
-					{addonAfter}
-				</div>
-			}
+			{this.renderAddon(addonAfter, "after", inputPrefix)}
 		</div>;
 	}
 }
