35a5e7c98957ceb2de6398dfd1a158fee2948eda
djChan97 <moldysaltedfish@gmail.com>
2020-03-30 07:32:13 +0800

    component: checkboxGroup optimize code and support size

--- src/components/checkbox/checkboxGroup.js
+++ src/components/checkbox/checkboxGroup.js
@@ -12,73 +12,35 @@ class CheckboxGroup extends React.Component {
 		};
 	}
 
-	/**
-	 *
-	 * @param nextProps
-	 * @param nextContext
-	 * @constructor
-	 */
 	UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
-		if (!this.isArray(nextProps.options)) return;
-		let formatArray;
 		/** 只针对 value 更改的情况 */
 		if (nextProps.value && this.isArray(nextProps.value)) {
-			formatArray = this.formatOptions(nextProps.options, nextProps.value, nextProps.disabled);
-		}
-		this.setState({
-			formatOptions: formatArray
-		});
-		this.getSelectedValue(formatArray);
-	}
-
-	componentDidMount() {
-		const {options, value, defaultValue, disabled} = this.props;
-		let formatArray;
-		if (this.isArray(value)) {
+			let formatArray = this.formatOptions(nextProps.options, nextProps.value, nextProps.disabled, nextProps.size);
 			this.setState({
-				controlMode: true
+				formatOptions: formatArray
 			});
-			formatArray = this.formatOptions(options, value, disabled);
-		} else {
-			formatArray = this.formatOptions(options, defaultValue, disabled);
+			this.getSelectedValue(formatArray);
 		}
-		this.setState({
-			formatOptions: formatArray,
-			selectedValue: this.getSelectedValue(formatArray)
-		});
 	}
 
-	/**
-	 *
-	 * @param selectedValue（非受控状态的已选择值）
-	 */
 	emitCallbackAndFormatOptions = (selectedValue) => {
-		const {options, disabled} = this.props;
+		const {options, disabled, size} = this.props;
 		this.props.onChange(selectedValue);
 		this.setState({
-			formatOptions: this.formatOptions(options, selectedValue, disabled)
+			formatOptions: this.formatOptions(options, selectedValue, disabled, size)
 		});
 	};
 
-	/**
-	 *
-	 * @param array
-	 * @returns {arg is any[]}
-	 */
 	isArray = (array) => {
 		return Array.isArray(array);
 	};
 
-	/**
-	 *
-	 * @param e
-	 */
 	childChange = (e) => {
 		const value = e.target.value;
 		const checked = e.target.checked;
 		const {selectedValue, controlMode} = this.state;
 		if (controlMode) {
-			/** 受状态下直接触发回调 */
+			/** 受控状态下直接触发回调 */
 			if (checked) {
 				this.props.onChange(selectedValue.concat(value));
 			} else {
@@ -105,23 +67,17 @@ class CheckboxGroup extends React.Component {
 		}
 	};
 
-	/**
-	 *
-	 * @param options
-	 * @param selectedValue（ 受控状态或者非受控状态的已选择值）
-	 * @param disabled
-	 * @returns {[]}
-	 */
-	formatOptions = (options, selectedValue, disabled) => {
+	formatOptions = (options, selectedValue, disabled, size) => {
 		const formatArray = [];
+		if (!this.isArray(options)) return formatArray;
 		for (let i = 0; i < options.length; i++) {
 			if (typeof options[i] === "string") {
 				const temp = {};
-				temp.label = options[i];
-				temp.value = options[i];
-				temp.disabled = disabled;
+				temp.value = temp.label = options[i];
+				temp.size = size;
+				temp.disabled = !!disabled;
 				temp.onChange = this.childChange;
-				temp.selected = selectedValue.includes(temp.value);
+				this.isArray(selectedValue) ? (temp.selected = !!selectedValue.includes(temp.value)) : (temp.selected = false);
 				formatArray.push(temp);
 			} else if (typeof options[i] === "object") {
 				if (Object.keys(options[i]).length !== 0) {
@@ -132,22 +88,18 @@ class CheckboxGroup extends React.Component {
 						temp.label = options[i].label;
 					}
 					temp.value = options[i].value;
+					temp.size = size;
 					temp.onChange = this.childChange;
-					options[i].disabled === undefined && (temp.disabled = disabled);
-					options[i].disabled !== undefined && (temp.disabled = options[i].disabled);
+					options[i].disabled === undefined && (temp.disabled = !!disabled);
+					options[i].disabled !== undefined && (temp.disabled = !!options[i].disabled);
+					this.isArray(selectedValue) ? (temp.selected = !!selectedValue.includes(temp.value)) : (temp.selected = false);
 					formatArray.push(temp);
-					temp.selected = selectedValue.includes(temp.value);
 				}
 			}
 		}
 		return formatArray;
 	};
 
-	/**
-	 *
-	 * @param formatArray
-	 * @returns {[]}
-	 */
 	getSelectedValue = (formatArray) => {
 		let temp = [];
 		for (let i = 0; i < formatArray.length; i++) {
@@ -156,8 +108,30 @@ class CheckboxGroup extends React.Component {
 		this.setState({
 			selectedValue: temp
 		});
-		return temp;
 	};
+
+	componentDidMount() {
+		const {options, value, defaultValue, disabled, size} = this.props;
+		let formatArray;
+		if (this.isArray(value) || value !== undefined) {
+			this.setState({
+				controlMode: true
+			});
+			if (this.isArray(value)) {
+				formatArray = this.formatOptions(options, value, disabled, size);
+			} else {
+				formatArray = this.formatOptions(options, [], disabled, size);
+			}
+		} else {
+			let val = defaultValue;
+			if (!this.isArray(defaultValue)) val = [];
+			formatArray = this.formatOptions(options, val, disabled, size);
+		}
+		this.getSelectedValue(formatArray);
+		this.setState({
+			formatOptions: formatArray,
+		});
+	}
 
 	render() {
 		const {formatOptions, controlMode} = this.state;
@@ -166,7 +140,8 @@ class CheckboxGroup extends React.Component {
 				key: index,
 				value: opt.value,
 				disabled: opt.disabled,
-				onChange: opt.onChange
+				onChange: opt.onChange,
+				size: opt.size
 			};
 			if (controlMode) {
 				attr["checked"] = opt.selected;
@@ -183,16 +158,24 @@ class CheckboxGroup extends React.Component {
 }
 
 CheckboxGroup.propTypes = {
-	options: PropTypes.array,
+	/** 可选项 */
+	options: PropTypes.array.isRequired,
+	/** 默认选择项（非控状态） */
 	defaultValue: PropTypes.array,
+	/** 选中项（受控状态） */
 	value: PropTypes.array,
+	/** 变化时回调函数 */
 	onChange: PropTypes.func,
+	/** 整组失效 */
 	disabled: PropTypes.bool,
+	/** 大小 */
+	size: PropTypes.oneOf(["small", "medium", "large"])
 };
 
 CheckboxGroup.defaultProps = {
 	disabled: false,
 	options: [],
+	size: "medium",
 	onChange: () => {
 	}
 };
