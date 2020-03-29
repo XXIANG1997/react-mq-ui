cfd2be359512012f6c4309a375ec9a9a62fe9273
djChan97 <moldysaltedfish@gmail.com>
2020-03-30 07:32:44 +0800

    storybook: add checkbox section

---
+++ src/stories/code/checkbox.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import MqCheckbox from "../../components/checkbox";
+
+const Checkbox = () => {
+	const [checked1, onChange1] = useState(true);
+	const [checked2, onChange2] = useState(["apple", "orange"]);
+	return <div>
+		<h2>basic usage</h2>
+		<MqCheckbox onChange={(e) => console.log(e.target.checked)}
+		            defaultChecked={true}>banana(unControl)</MqCheckbox>
+		<MqCheckbox checked={checked1} onChange={(e) => onChange1(e.target.checked)}>banana(control)</MqCheckbox>
+		<MqCheckbox indeterminate={true}>indeterminate(only control
+			style)</MqCheckbox>
+		{/*<MqCheckbox.Group options={this.state.options} defaultValue={["apple", "orange"]}*/}
+		{/*                  onChange={(value) => console.log(value)}/>*/}
+		<h2>checkbox group</h2>
+		<h3>unControl</h3>
+		<MqCheckbox.Group options={["apple", "orange", "grape", "tomato"]} defaultValue={["apple"]}
+		                  onChange={(value) => console.log(value)}/>
+		<h3>control</h3>
+		<MqCheckbox.Group options={["apple", "orange", "grape", "banana"]} value={checked2}
+		                  onChange={(value) => onChange2(value)}/>
+		<h2>size</h2>
+		<MqCheckbox onChange={(e) => console.log(e.target.checked)}
+		            defaultChecked={true} size={"small"}>small</MqCheckbox>
+		<MqCheckbox
+			onChange={(e) => console.log(e.target.checked)}
+			defaultChecked={true} size={"medium"}>medium</MqCheckbox>
+		<MqCheckbox
+			onChange={(e) => console.log(e.target.checked)}
+			defaultChecked={true} size={"large"}>large</MqCheckbox>
+		<MqCheckbox.Group options={["apple", "orange", "grape", "tomato"]} size={"small"} defaultValue={["apple"]}
+		                  onChange={(value) => console.log(value)}/>
+		<MqCheckbox.Group options={["apple", "orange", "grape", "tomato"]} size={"medium"} defaultValue={["apple"]}
+		                  onChange={(value) => console.log(value)}/>
+		<MqCheckbox.Group options={["apple", "orange", "grape", "tomato"]} size={"large"} defaultValue={["apple"]}
+		                  onChange={(value) => console.log(value)}/>
+		<h2>disabled</h2>
+		<MqCheckbox onChange={(e) => console.log(e.target.checked)} disabled={true}>disabled</MqCheckbox>
+		<MqCheckbox onChange={(e) => console.log(e.target.checked)} defaultChecked={true}
+		            disabled={true}>disabled</MqCheckbox>
+		<MqCheckbox onChange={(e) => console.log(e.target.checked)} indeterminate={true}
+		            disabled={true}>disabled</MqCheckbox>
+		<h3>disabled single item</h3>
+		<MqCheckbox.Group options={[{value: "apple", label: "disabled", disabled: true}, "orange", "grape", "tomato"]}
+		                  size={"medium"}
+		                  defaultValue={["apple"]}
+		                  onChange={(value) => console.log(value)}/>
+		<h3>disabled all</h3>
+		<MqCheckbox.Group options={[{value: "apple", disabled: true}, "orange", "grape", "tomato"]} disabled={true}
+		                  size={"medium"}
+		                  defaultValue={["apple", "orange"]}
+		                  onChange={(value) => console.log(value)}/>
+	</div>;
+};
+
+export default Checkbox;
--- src/stories/dataEntry.stories.js
+++ src/stories/dataEntry.stories.js
@@ -2,8 +2,10 @@ import { storiesOf } from "@storybook/react";
 import Input from "./code/input";
 import NumberInput from "./code/numberInput";
 import Radio from "./code/raido";
+import Checkbox from "./code/checkbox";
 
 storiesOf("数据录入", module)
 	.add("input 输入框", Input)
 	.add("numberInput 数字输入框", NumberInput)
-	.add("radio 单选按钮", Radio);
+	.add("radio 单选按钮", Radio)
+	.add("checkbox 复选框", Checkbox);
